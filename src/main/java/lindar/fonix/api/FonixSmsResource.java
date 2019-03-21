5a2b02a35068c9b7ca698635b94c42182321e711
Steven Hills <steven.hills@gmail.com>
2019-03-21 18:34:50 +0000

    Fix NPE when parsing delivery report with no received time or status time

--- src/main/java/lindar/fonix/api/FonixSmsResource.java
+++ src/main/java/lindar/fonix/api/FonixSmsResource.java
@@ -23,9 +23,6 @@ import java.util.Map;
 
 /**
  * Api for dealing with SMS on the Fonix Platform
- *
- * Created by Steven on 09/03/2017.
- *
  */
 @Slf4j
 public class FonixSmsResource extends BaseFonixResource {
@@ -61,7 +58,7 @@ public class FonixSmsResource extends BaseFonixResource {
     /**
      * Constructor
      *
-     * @param apiKey apikey provided by fonix to authenticate with their services
+     * @param apiKey    apikey provided by fonix to authenticate with their services
      * @param dummyMode when true api calls will run in dummy mode (no action taken) unless the dummy mode is otherwise specified
      */
     public FonixSmsResource(String apiKey, boolean dummyMode) {
@@ -82,17 +79,15 @@ public class FonixSmsResource extends BaseFonixResource {
     /**
      * Send SMS
      *
-     * @param to mobile number to send sms to in E.164 format (International format)
-     * @param from the originator the sms is from, must either be a pre-registered shortcode
-     *             or a alphanumeric sender ID (max 11 alphanumeric characters)
-     * @param body the content of the sms to be sent
+     * @param to        mobile number to send sms to in E.164 format (International format)
+     * @param from      the originator the sms is from, must either be a pre-registered shortcode
+     *                  or a alphanumeric sender ID (max 11 alphanumeric characters)
+     * @param body      the content of the sms to be sent
      * @param dummyMode when dummyMode is true the message will not actually be sent
-     *
-     * @throws FonixBadRequestException if there is a problem with the values sent in the request
-     * @throws FonixNotAuthorizedException if authentication fails
-     * @throws FonixUnexpectedErrorException if a unexpected error occurs on the Fonix platform
-     *
      * @return details of the sms sent
+     * @throws FonixBadRequestException      if there is a problem with the values sent in the request
+     * @throws FonixNotAuthorizedException   if authentication fails
+     * @throws FonixUnexpectedErrorException if a unexpected error occurs on the Fonix platform
      */
     public SendSmsResponse sendSms(String to, String from, String body, boolean dummyMode) throws FonixException {
 
@@ -101,7 +96,7 @@ public class FonixSmsResource extends BaseFonixResource {
         formParams.add(new BasicNameValuePair("ORIGINATOR", from));
         formParams.add(new BasicNameValuePair("NUMBERS", to));
 
-        if(dummyMode){
+        if (dummyMode) {
             formParams.add(new BasicNameValuePair("DUMMY", "yes"));
         }
 
@@ -127,16 +122,14 @@ public class FonixSmsResource extends BaseFonixResource {
     /**
      * Charge SMS
      *
-     * @param to mobile number to send sms to in E.164 format (International format)
-     * @param from the originator the sms is from, must be a pre-registered shortcode for the specific charge amount
-     * @param body the content of the sms to be sent, content must not be more then 1 sms (aka 160 characters)
+     * @param to        mobile number to send sms to in E.164 format (International format)
+     * @param from      the originator the sms is from, must be a pre-registered shortcode for the specific charge amount
+     * @param body      the content of the sms to be sent, content must not be more then 1 sms (aka 160 characters)
      * @param dummyMode when dummyMode is true the message will not actually be sent
-     *
-     * @throws FonixBadRequestException if there is a problem with the values sent in the request
-     * @throws FonixNotAuthorizedException if authentication fails
-     * @throws FonixUnexpectedErrorException if a unexpected error occurs on the Fonix platform
-     *
      * @return details of the sms sent
+     * @throws FonixBadRequestException      if there is a problem with the values sent in the request
+     * @throws FonixNotAuthorizedException   if authentication fails
+     * @throws FonixUnexpectedErrorException if a unexpected error occurs on the Fonix platform
      */
     public ChargeSmsResponse chargeSms(String to, String from, String body, boolean dummyMode) throws FonixException {
 
@@ -145,7 +138,7 @@ public class FonixSmsResource extends BaseFonixResource {
         formParams.add(new BasicNameValuePair("ORIGINATOR", from));
         formParams.add(new BasicNameValuePair("NUMBERS", to));
 
-        if(dummyMode){
+        if (dummyMode) {
             formParams.add(new BasicNameValuePair("DUMMY", "yes"));
         }
 
@@ -163,10 +156,10 @@ public class FonixSmsResource extends BaseFonixResource {
      * Parse map of values (from request) into a delivery report
      *
      * @param mapParameters should contain the parameters sent from the fonix server
-     **
+     *                      *
      * @return the fully build delivery report
      */
-    public DeliveryReport parseDeliveryReport(Map<String, String> mapParameters){
+    public DeliveryReport parseDeliveryReport(Map<String, String> mapParameters) {
         DeliveryReport deliveryReport = new DeliveryReport();
 
         deliveryReport.setMobileNumber(mapParameters.get(DR_MOBILE_NUMBER));
@@ -177,22 +170,26 @@ public class FonixSmsResource extends BaseFonixResource {
         deliveryReport.setStatusCode(mapParameters.get(DR_STATUS_CODE));
 
 
-        if(NumberUtils.isParsable(mapParameters.get(DR_DURATION))){
+        if (NumberUtils.isParsable(mapParameters.get(DR_DURATION))) {
             deliveryReport.setDuration(Integer.parseInt(mapParameters.get(DR_DURATION)));
         }
 
-        if(NumberUtils.isParsable(mapParameters.get(DR_RETRY_COUNT))){
+        if (NumberUtils.isParsable(mapParameters.get(DR_RETRY_COUNT))) {
             deliveryReport.setRetryCount(Integer.parseInt(mapParameters.get(DR_RETRY_COUNT)));
         }
 
         try {
-            deliveryReport.setStatusTime(parseStatusTime.parse(mapParameters.get(DR_STATUS_TIME)));
+            if (mapParameters.containsKey(DR_STATUS_TIME)) {
+                deliveryReport.setStatusTime(parseStatusTime.parse(mapParameters.get(DR_STATUS_TIME)));
+            }
         } catch (ParseException e) {
             log.error("unable to parse status time from string {}", mapParameters.get(DR_STATUS_TIME));
         }
 
         try {
-            deliveryReport.setReceiveTime(parseStatusTime.parse(mapParameters.get(DR_RECEIVE_TIME)));
+            if (mapParameters.containsKey(DR_RECEIVE_TIME)) {
+                deliveryReport.setReceiveTime(parseStatusTime.parse(mapParameters.get(DR_RECEIVE_TIME)));
+            }
         } catch (ParseException e) {
             log.error("unable to parse receive time from string {}", mapParameters.get(DR_RECEIVE_TIME));
         }
